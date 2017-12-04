c9d7f09c33cd314d1fc931f54a124ba4b8c5ce82
tanzhiwen <tanzhiwen@xiaohong.local>
2017-12-04 18:07:57 +0800

    commited: 创建项目

---
+++ McMusicFly/McMusicFly/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  McMusicFly
+//
+//  Created by TANZHIWEN on 2017/12/4.
+//  Copyright © 2017年 TANZHIWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
