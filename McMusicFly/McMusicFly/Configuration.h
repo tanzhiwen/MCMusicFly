4d41c421f258e730a399b658c4d60ff6c83427b2
TANZHIWEN <tanzhiwen@xiaohong.local>
2017-12-04 19:21:21 +0800

    commited: 架构搭建一下

---
+++ McMusicFly/McMusicFly/Configuration.h
@@ -0,0 +1,13 @@
+//
+//  Configuration.h
+//  McMusicFly
+//
+//  Created by TANZHIWEN on 2017/12/4.
+//  Copyright © 2017年 TANZHIWEN. All rights reserved.
+//
+
+#ifndef Configuration_h
+#define Configuration_h
+
+
+#endif /* Configuration_h */
